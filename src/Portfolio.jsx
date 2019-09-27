98a476303acdc6be36bf1c034026053ed2319676
Sam Robertson <samrob.nz@gmail.com>
2019-09-28 02:02:40 +1200

    srux start

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,18 +1,42 @@
 import React, {Component} from 'react';
-
+import HorizontalScroll from 'react-scroll-horizontal'
 class Portfolio extends Component {
 
 
 render (){
 
         return (
-            <>
-            <div className="pWrap">
+
+            <container className="pWrap">
+                    <HorizontalScroll
+                        // pageLock      = { bool }
+                        // reverseScroll = { bool }
+                        // style         = { object }
+                        // config        = {{ stiffness: int, damping: int }}
+                        className="work"
+                        // animValues    = { int }
+                        >
+                        <section className="pSection">
+                            <row className="row1"></row>
+                            <row className="row2"></row>
+                        </section>
+                        <section className="pSection">
+                            <row className="row3"></row>
+                            <row className="row4"></row>
+                        </section>
+                        <section className="pSection">
+                            <row className="row1"></row>
+                            <row className="row2"></row>
+                        </section>
+                        <section className="pSection">
+                            <row className="row3"></row>
+                            <row className="row4"></row>
+                        </section>
+                    </HorizontalScroll>
 
 
+            </container>
 
-            </div>
-            </>
         )
     }
 }
